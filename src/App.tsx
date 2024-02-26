16c6fc318a549c88718e35ebc2fd39f9153efdfe
Mitra Singh <mitra.coder@gmail.com>
2024-02-26 11:35:01 -0500

    imported TodoTask component, mapping through state todoList which returns TodoTask component for each task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FC, ChangeEvent, useState } from "react";
 import "./App.css";
 import { Task } from "./Types";
+import { TodoTask } from "./components/TodoTask";
 
 const App: FC = () => {
 	const [task, setTask] = useState<string>("");
@@ -18,7 +19,8 @@ const App: FC = () => {
 	const addTask = (): void => {
 		const newTask = { taskName: task, deadline: deadline };
 		setTodoList([...todoList, newTask]);
-		console.log(todoList);
+		setTask("");
+		setDeadline(0);
 	};
 
 	return (
@@ -29,18 +31,24 @@ const App: FC = () => {
 						type="text"
 						placeholder="Task..."
 						name="task"
+						value={task}
 						onChange={handleChange}
 					/>
 					<input
 						type="number"
 						placeholder="Deadline (in Days)"
 						name="deadline"
+						value={deadline}
 						onChange={handleChange}
 					/>
 				</div>
 				<button onClick={addTask}>Add Task</button>
 			</div>
-			<div className="todoList"></div>
+			<div className="todoList">
+				{todoList.map((task: Task, key: number) => {
+					return <TodoTask key={key} task={task} />;
+				})}
+			</div>
 		</div>
 	);
 };

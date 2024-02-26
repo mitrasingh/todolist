415468e65bea99f3708bd451d6e6a1ee6ec190f2
Mitra Singh <mitra.coder@gmail.com>
2024-02-26 11:20:36 -0500

    added onChange click event functionality and imported types file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import { FC } from "react";
+import { FC, ChangeEvent, useState } from "react";
 import "./App.css";
+import { Task } from "./Types";
 
 const App: FC = () => {
+	const [task, setTask] = useState<string>("");
+	const [deadline, setDeadline] = useState<number>(0);
+	const [todoList, setTodoList] = useState<Task[]>([]);
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+		if (event.target.name === "task") {
+			setTask(event.target.value);
+		} else {
+			setDeadline(Number(event.target.value));
+		}
+	};
+
+	const addTask = (): void => {
+		const newTask = { taskName: task, deadline: deadline };
+		setTodoList([...todoList, newTask]);
+		console.log(todoList);
+	};
+
 	return (
 		<div className="App">
 			<div className="header">
 				<div className="inputContainer">
-					<input type="text" placeholder="Task..." />
-					<input type="number" placeholder="Deadline (in Days)" />
+					<input
+						type="text"
+						placeholder="Task..."
+						name="task"
+						onChange={handleChange}
+					/>
+					<input
+						type="number"
+						placeholder="Deadline (in Days)"
+						name="deadline"
+						onChange={handleChange}
+					/>
 				</div>
-				<button>Add Task</button>
+				<button onClick={addTask}>Add Task</button>
 			</div>
 			<div className="todoList"></div>
 		</div>

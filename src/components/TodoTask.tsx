4fc4dfe9a6eaaaa7a0248d90b516a148670c7143
Mitra Singh <mitra.coder@gmail.com>
2024-02-26 12:11:57 -0500

    completeTask function added to component

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -2,12 +2,17 @@ import { Task } from "../Types";
 
 type TodoTaskProps = {
 	task: Task;
+	completeTask(taskNameToDelete: string): void;
 };
 
-export const TodoTask = ({ task }: TodoTaskProps) => {
+export const TodoTask = ({ task, completeTask }: TodoTaskProps) => {
 	return (
-		<div>
-			{task.taskName} {task.deadline}
+		<div className="task">
+			<div className="content">
+				<span>{task.taskName}</span>
+				<span>{task.deadline}</span>
+			</div>
+			<button onClick={() => completeTask(task.taskName)}>X</button>
 		</div>
 	);
 };
